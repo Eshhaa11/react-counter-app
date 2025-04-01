56dad1ca580a778142afc510e35cf161d005c6a5
Esha Patel <Patellesha11@gmail.com>
2025-04-01 12:16:12 +0300

    defined decreaseCount

--- src/Counter.js
+++ src/Counter.js
@@ -5,7 +5,9 @@ function counter () {
 
     const [count, setCount] = useState(0)
 
-    const increaseCount = () => setCount(count + 1)
+    const increaseCount = () => setCount(count + 1);
+    const decreaseCount = () => setCount(count - 1);
+    
 
     return (
         <div>

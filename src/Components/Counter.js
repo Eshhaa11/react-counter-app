dc6a67fcf64b7a8bea8196d430e828224c893599
Esha Patel <Patellesha11@gmail.com>
2025-04-01 12:38:36 +0300

    added comp

---
+++ src/Components/Counter.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function counter() {
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
+  const resetCount = () => setCount(0);
+
+  return (
+    <div>
+      <h1>COUNTER APP</h1>
+      <h2>{count}</h2>
+
+      <div className="button-container">
+        <button onClick={increaseCount}>+1</button>
+        <button onClick={decreaseCount}>-1</button>
+        <button onClick={resetCount}>Reset</button>
+      </div>
+    </div>
+  );
+}
